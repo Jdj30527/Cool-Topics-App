4643cf5a5c194a15e908cb7ab48c6227cffaa472
John Jefferies <johnj17@vt.edu>
2018-04-22 11:16:46 -0400

    First commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by John on 3/27/18.
+//  Copyright © 2018 John Jefferies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
+    }
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+}
+
